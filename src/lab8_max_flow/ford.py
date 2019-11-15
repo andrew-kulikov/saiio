016faf4e23c12dfffa6780696c2600c629194c0e
andrei.kulikou <andrei.kulikou@soft-fx.lv>
2019-11-15 12:35:04 +0300

    Refactor ford algorithm

--- src/lab8_max_flow/ford.py
+++ src/lab8_max_flow/ford.py
@@ -35,6 +35,49 @@ def get_start_flow(g):
     return g1
 
 
+def find_alpha(s, t, cur_flow, g, g_forward):
+    cur = t
+    alpha = np.inf
+
+    # минимальный увеличивающий поток
+    while cur != s:
+        prev, direction = g[cur]
+
+        if direction == DIRECTION_FORWARD:
+            allowed = g_forward[prev][cur] - cur_flow[prev][cur]
+        else:
+            allowed = cur_flow[cur][prev]
+
+        alpha = min(alpha, allowed)
+        cur = prev
+
+    return alpha
+
+
+def recalculate_path(s, t, cur_flow, g, alpha):
+    cur = t
+    # обновляем путь
+    while cur != s:
+        prev, direction = g[cur]
+
+        if direction == DIRECTION_FORWARD:
+            cur_flow[prev][cur] += alpha
+        else:
+            cur_flow[cur][prev] -= alpha
+
+        cur = prev
+
+    return cur_flow
+
+
+def restore_flow(s, t, cur_flow, g, g_forward):
+    alpha = find_alpha(s, t, cur_flow, g, g_forward)
+
+    cur_flow = recalculate_path(s, t, cur_flow, g, alpha)
+
+    return alpha, cur_flow
+
+
 def ford(pairs, s, t):
     g_forward = build_graph(pairs, t)
     g_backward = build_parents_graph(pairs, s)
@@ -44,6 +87,7 @@ def ford(pairs, s, t):
     flow = 0
 
     while True:
+        # step 1 - инициализация
         ic = 1
         it = 1
         L = [s]
@@ -53,7 +97,7 @@ def ford(pairs, s, t):
         i = s
 
         while True:
-            # step 2
+            # step 2 - обход всех исходящих непомеченных вершин
             for j, weight in g_forward[i].items():
                 if j in L or cur_flow[i][j] >= weight:
                     continue
@@ -65,7 +109,7 @@ def ford(pairs, s, t):
 
                 L.append(j)
 
-            # step 3
+            # step 3 - обход всех входящих непомеченных вершин
             for j, weight in g_backward[i].items():
                 if j in L or cur_flow[j][i] == 0:
                     continue
@@ -76,49 +120,24 @@ def ford(pairs, s, t):
                 p_inv[it] = j
 
                 L.append(j)
-            
-            # step 4
+
+            # step 4 - если дошли до стока
             if t in L:
                 break
 
-            # step 5
+            # step 5 - переходим к следующей вершине
             ic += 1
             if ic not in p_inv:
                 break
 
             i = p_inv[ic]
-        
-        # restore flow
+
+        # выходим, если не наашли увеличивающего пути
         if t not in g:
             return flow
 
-        cur = t
-        alpha = np.inf
+        alpha, cur_flow = restore_flow(s, t, cur_flow, g, g_forward)
 
-        # минимальный увеличивающий поток
-        while cur != s:
-            prev, direction = g[cur]
-
-            if direction == DIRECTION_FORWARD:
-                allowed = g_forward[prev][cur] - cur_flow[prev][cur]
-            else:
-                allowed = cur_flow[cur][prev]
-        
-            alpha = min(alpha, allowed)
-            cur = prev
-
-        cur = t
-        # обновляем путь
-        while cur != s:
-            prev, direction = g[cur]
-
-            if direction == DIRECTION_FORWARD:
-                cur_flow[prev][cur] += alpha
-            else:
-                cur_flow[cur][prev] -= alpha
-            
-            cur = prev
-        
         flow += alpha
 
     return flow
