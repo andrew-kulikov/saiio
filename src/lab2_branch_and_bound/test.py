26b1af12fa77ef1bdd8c67c5e681a519acec7a8d
Andrew <andrew.1.kulikov@gmail.com>
2019-11-23 01:18:19 +0300

    Add template for branch and bound

---
+++ src/lab2_branch_and_bound/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class BranchAndBoundTestCase(unittest.TestCase):
+    def test_task1(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
